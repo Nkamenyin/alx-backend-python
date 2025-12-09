ae98f3b9be1336afa6ca65e9a49d4c25ce48fb4f
Nkamenyin <valentineeyet01@gmail.com>
2025-12-09 22:17:30 +0800

    Implementation of the tearDownClass class upgraded again

--- 0x03-Unittests_and_integration_tests/fixtures.py
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -948,3 +948,13 @@ TEST_PAYLOAD = [
     ['dagger', 'kratu', 'traceur-compiler', 'firmata.py'],
   )
 ]
+
+#added
+
+org_payload = {"repos_url": "https://api.github.com/orgs/google/repos"}
+repos_payload = [
+    {"name": "repo1", "license": {"key": "apache-2.0"}},
+    {"name": "repo2", "license": {"key": "mit"}},
+]
+expected_repos = ["repo1", "repo2"]
+apache2_repos = ["repo1"]
